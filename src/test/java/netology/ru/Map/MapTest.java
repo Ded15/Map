6f336400513915488d44760394840126b53e81bb
Julia Hardikova <iylia.hardikova@mail.ru>
2023-01-10 18:41:02 +0300

    Initial commit

---
+++ src/test/java/netology/ru/Map/MapTest.java
@@ -0,0 +1,42 @@
+package netology.ru.Map;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MapTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUppAll(){
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() { driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void  tearDown(){
+        driver.quit();
+        driver=null;
+    }
+
+    @Test
+    void positiveStringValidation() {
+        driver.get ("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванова Нина");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79524064315");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button__content")).click();
+        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals(expected,actual);
+    }
+}
